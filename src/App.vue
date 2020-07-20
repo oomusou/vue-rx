fe67a896c09b1f327c0c19dc8091ddb20c6e0d99
oomusou xiao <oomusou@gmail.com>
2020-07-20 21:52:58 +0800

    concat

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
   <div>
-    <input type="text" v-stream:input="input$">
-    {{ message$ }}
+    <button v-stream:click="click$">Show Message</button>
+    <div>{{ message$ }}</div>
   </div>
 </template>
 
 <script>
-import { pluck, scan } from 'rxjs/operators'
+import { concat, EMPTY } from 'rxjs'
+import { startWith, delay, switchMapTo } from 'rxjs/operators'
+
+let delayMsg$ = x => EMPTY.pipe(
+  startWith(x),
+  delay(1000)
+)
 
 let subscriptions = function() {
-  let message$ = this.input$.pipe(
-    pluck('event', 'data'),
-    scan((a, x) => a + x)
+  let slogan$ = concat(
+    delayMsg$('Get Ready!'),
+    delayMsg$(3),
+    delayMsg$(2),
+    delayMsg$(1),
+    delayMsg$('Go!')
+  )
+
+  let message$ = this.click$.pipe(
+    switchMapTo(slogan$)
   )
 
   return { message$ }
 }
 
 export default {
-  name: 'app',
+  name:'App',
   domStreams: [
-    'input$'
+    'click$'
   ],
   subscriptions
 }
 </script>
+
